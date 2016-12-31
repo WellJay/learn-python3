928f52214c52e801c3f3be6b916bd0aa79ce658c
文杰 <1569925@qq.com>
2016-12-31 11:22:46 +0800

    init project

---
+++ basic/com/welljay/DataTypeAndVariables.py
@@ -0,0 +1,51 @@
+'''
+数据类型和变量
+- 整数   eg:1,100,-8888
+- 浮点数 eg:1.23,-8,88
+- 字符串 eg:"str",'test'
+- 布尔   eg:True,False,2 > 1,1 == 1
+- 空值   None
+- 变量   eg:num = 5, age = 20
+- 常量   eg:PI=3.14159265359
+'''
+
+
+#tips:转义字符\
+print('I\'m ok.')
+# I'm ok.
+
+print('I\'m learning\nPython')
+# I'm learning
+# Python
+
+print('\\\\')
+# \\
+
+#tips:如果字符串内部有很多换行，用\n写在一行里不好阅读，为了简化，Python允许用'''...'''的格式表示多行内容，
+print('''line1
+...line2
+...line3''')
+# lin1
+# ...line2
+# ...line3
+
+print(True)
+# True
+print(2 > 1)
+# True
+print(True and False)
+# False
+
+#tips:条件判断
+age = 20
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+# adult
+
+a = 'ABC'
+b = a
+a = 'DEF'
+print(b)
+# ABC
---
+++ basic/com/welljay/StrAndEncoding.py
@@ -0,0 +1,25 @@
+'''
+字符串和编码
+- 对于单个字符的编码，Python提供了ord()函数获取字符的整数表示，chr()函数把编码转换为对应的字符
+- 由于Python的字符串类型是str，在内存中以Unicode表示，一个字符对应若干个字节。如果要在网络上传输，或者保存到磁盘上，就需要把str变为以字节为单位的bytes。
+  Python对bytes类型的数据用带b前缀的单引号或双引号表示
+- 以Unicode表示的str通过encode()方法可以编码为指定的bytes
+'''
+
+print("中文字符串")
+# 中文字符串
+
+print(ord('A'))
+# 65
+print(chr(97))
+# a
+
+#tips:要注意区分'ABC'和b'ABC'，前者是str，后者虽然内容显示得和前者一样，但bytes的每个字符都只占用一个字节
+x = b'ABC'
+print(x)
+# b'ABC'
+
+print('TEST'.encode('ascii'))
+# b'TEST'
+print('中文'.encode('utf-8'))
+# b'\xe4\xb8\xad\xe6\x96\x87'
